98108777ccb25ac9ad6bf5e6b6664b2c894751b5
FE_juncci <81576126+Debuging-JunSeoPark@users.noreply.github.com>
2025-09-18 14:16:46 +0900

    Feature/console UI refactor (#2)
    * refactor: 패키지 구조 정리(domain/service/ui)로 코드 책임별 분리
    
    * refactor: Main을 단일 책임 구조로 수정하여 ConsoleController로 UI 로직 이관

---
+++ WEEK02/src/ui/ConsoleView.java
@@ -0,0 +1,43 @@
+package ui;
+public class ConsoleView {
+
+    // ===== 메인 메뉴 =====
+    public void showMainMenu() {
+        System.out.println("\n=== 도서관 시스템 ===");
+        System.out.println("1. 사용자");
+        System.out.println("2. 관리자");
+        System.out.println("0. 종료");
+        System.out.println("===============");
+        System.out.print("선택: ");
+    }
+
+    public void showInvalidInput() {
+        System.out.println("잘못된 입력입니다.");
+    }
+
+    public void showExit() {
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    // ===== 사용자 메뉴 =====
+    public void showUserMenu() {
+        System.out.println("\n<<< 사용자 메뉴 >>>");
+        System.out.println("1. 대여");
+        System.out.println("2. 반납");
+        System.out.println("3. 내 대여 현황");
+        System.out.println("4. 대출 가능 목록");
+        System.out.println("0. 메인으로 돌아가기");
+        System.out.print("선택: ");
+    }
+
+    // ===== 관리자 메뉴 =====
+    public void showAdminMenu() {
+        System.out.println("\n[[[ 관리자 메뉴 ]]]");
+        System.out.println("1. 책 등록");
+        System.out.println("2. 책 삭제");
+        System.out.println("3. 책 수정");
+        System.out.println("4. 전체 책 목록");
+        System.out.println("0. 메인으로 돌아가기");
+        System.out.print("선택: ");
+    }
+}
